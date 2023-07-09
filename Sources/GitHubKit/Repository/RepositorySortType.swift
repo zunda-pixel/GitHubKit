efee94b67c6180d93d9ea122a2dce2abb89663c0
zunda <zunda.dev@gmail.com>
2023-07-10 02:05:55 +0900

    add repositories

---
+++ Sources/GitHubKit/Repository/RepositorySortType.swift
@@ -0,0 +1,12 @@
+//
+//  RepositorySortType.swift
+//
+
+import Foundation
+
+public enum RepositorySortType: String {
+  case created
+  case updated
+  case pushed
+  case fullName = "full_name"
+}
