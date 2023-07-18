501affb172e44b1544582daaf908008a15e7def3
zunda <zunda.dev@gmail.com>
2023-07-19 02:31:54 +0900

    rename to repository

--- Sources/GitHubKit/Data/Branch/Branch.swift
+++ Sources/GitHubKit/Data/Branch/Branch.swift
@@ -9,19 +9,27 @@ public struct Branch: Codable, Sendable, Hashable {
   public let ref: String
   public let sha: String
   public let user: User?
-  public let repo: Repository?
+  public let repository: Repository?
   
   public init(
     label: String?,
     ref: String,
     sha: String,
     user: User?,
-    repo: Repository?
+    repository: Repository?
   ) {
     self.label = label
     self.ref = ref
     self.sha = sha
     self.user = user
-    self.repo = repo
+    self.repository = repository
+  }
+  
+  private enum CodingKeys: String, CodingKey {
+    case label
+    case ref
+    case sha
+    case user
+    case repository = "repo"
   }
 }
