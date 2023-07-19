faafd3f95297fd31138616e9c5842e9f08f19325
zunda <zunda.dev@gmail.com>
2023-07-19 18:34:13 +0900

    add starringUsers

---
+++ Sources/GitHubKit/Repository/Stars.swift
@@ -0,0 +1,39 @@
+//
+//  Stars.swift
+//
+
+import Foundation
+import HTTPTypes
+
+extension GitHubAPI {
+  /// List starring Users
+  /// - Parameters:
+  ///   - ownerID: The account owner of the repository. The name is not case sensitive.
+  ///   - repositoryName: The name of the repository without the .git extension. The name is not case sensitive.
+  ///   - perPage: The number of results per page (max 100).
+  ///   - page: Page number of the results to fetch.
+  /// - Returns: [User]
+  public func starringUsers(
+    ownerID: String,
+    repositoryName: String,
+    perPage: Int = 30,
+    page: Int = 1
+  ) async throws -> [User] {
+    let path = "/repos/\(ownerID)/\(repositoryName)/stargazers"
+    let endpoint = baseURL.appending(path: path)
+    let method: HTTPRequest.Method = .get
+    let queries: [String: String] = [
+      "per_page": String(perPage),
+      "page": String(page)
+    ]
+    
+    let request = HTTPRequest(method: method, url: endpoint, queries: queries, headers: headers())
+    
+    let (data, _) = try await session.data(for: request)
+    
+    let decoder = JSONDecoder.github
+    let users = try decoder.decode([User].self, from: data)
+    
+    return users
+  }
+}
