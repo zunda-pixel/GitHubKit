692be725a1679d872dba55aab73bd93f6b811788
zunda <zunda.dev@gmail.com>
2023-08-01 02:18:27 +0900

    fix ContentType

--- Sources/GitHubData/Releases/ContentType.swift
+++ Sources/GitHubData/Releases/ContentType.swift
@@ -7,10 +7,11 @@ import Foundation
 extension Asset {
   public enum ContentType: String, Codable, Sendable {
     case zip = "application/zip"
-    case octetStream = "application/octet-stream"
-    case xTar = "application/x-tar"
-    case pgpSignature = "application/pgp-signature"
-    case xDebianPackage = "application/x-debian-package"
-    case xGzip = "application/x-gzip"
+    case octet_stream = "application/octet-stream"
+    case x_tar = "application/x-tar"
+    case pgp_signature = "application/pgp-signature"
+    case x_debian_package = "application/x-debian-package"
+    case x_gzip = "application/x-gzip"
+    case x_msdownload = "application/x-msdownload"
   }
 }
