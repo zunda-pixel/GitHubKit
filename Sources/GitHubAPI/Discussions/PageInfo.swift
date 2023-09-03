d049517ff6b89d2c61d7760363f6dd0ce4ac89d5
zunda <zunda.dev@gmail.com>
2023-09-04 01:23:22 +0900

    add PageInfo

---
+++ Sources/GitHubAPI/Discussions/PageInfo.swift
@@ -0,0 +1,12 @@
+//
+//  PageInfo.swift
+//
+
+import Foundation
+
+public struct PageInfo: Sendable, Decodable {
+  public let endCursor: String
+  public let hasNextPage: Bool
+  public let hasPreviousPage: Bool
+  public let startCursor : String
+}
