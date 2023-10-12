aa6e90e0d455c591344d558dcd85cdb6c973c655
zunda <47569369+zunda-pixel@users.noreply.github.com>
2023-10-12 22:57:19 +0900

    add followers, following

--- Sources/GitHubAPI/User/Followers/Followers.swift
+++ Sources/GitHubAPI/User/Followers/Followers.swift
@@ -6,6 +6,39 @@ import Foundation
 import HTTPTypes
 
 extension GitHubAPI {
+  /// List followers of the authenticated user
+  /// https://docs.github.com/en/rest/users/followers?apiVersion=2022-11-28#list-followers-of-the-authenticated-user
+  /// - Parameters:
+  ///   - perPage: The number of results per page (max 100).
+  ///   - page: Page number of the results to fetch.
+  /// - Returns: [User]
+  public func followers(
+    perPage: Int = 30,
+    page: Int = 1
+  ) async throws -> [User] {
+    let path = "/user/followers"
+    let endpoint = baseURL.appending(path: path)
+    let method: HTTPRequest.Method = .get
+
+    let queries: [String: String] = [
+      "per_page": String(perPage),
+      "page": String(page),
+    ]
+
+    let request = HTTPRequest(
+      method: method,
+      url: endpoint,
+      queries: queries,
+      headers: headers
+    )
+
+    let (data, _) = try await session.data(for: request)
+
+    let users = try decode([User].self, from: data)
+
+    return users
+  }
+
   /// Lists the people following the specified user.
   /// https://docs.github.com/en/rest/users/followers?apiVersion=2022-11-28#list-followers-of-a-user
   /// - Parameters:
