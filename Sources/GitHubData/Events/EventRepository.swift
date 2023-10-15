6c5cfbe05c9f5a0b66d06a6aaf28d9276ac03080
zunda <47569369+zunda-pixel@users.noreply.github.com>
2023-10-16 02:43:55 +0900

    add Event Model

---
+++ Sources/GitHubData/Events/EventRepository.swift
@@ -0,0 +1,23 @@
+//
+//  EventRepository.swift
+//
+
+import Foundation
+
+extension Event {
+  public struct Repository: Codable, Sendable, Hashable {
+    public let id: Int
+    public let name: String
+    public let url: URL
+    
+    public init(
+      id: Int,
+      name: String,
+      url: URL
+    ) {
+      self.id = id
+      self.name = name
+      self.url = url
+    }
+  }
+}
---
+++ Sources/GitHubData/Events/EventUser.swift
@@ -0,0 +1,41 @@
+//
+//  EventUser.swift
+//
+
+import Foundation
+
+extension Event {
+  public struct User: Codable, Sendable, Hashable {
+    public let id: Int
+    public let userID: String
+    public let userName: String?
+    public let gravatarID: String
+    public let url: URL
+    public let avatarURL: String
+    
+    private enum CodingKeys: String, CodingKey {
+      case id
+      case userID = "login"
+      case userName = "display_login"
+      case gravatarID = "gravatar_id"
+      case url
+      case avatarURL = "avatar_url"
+    }
+    
+    public init(
+      id: Int,
+      userID: String,
+      userName: String?,
+      gravatarID: String,
+      url: URL,
+      avatarURL: String
+    ) {
+      self.id = id
+      self.userID = userID
+      self.userName = userName
+      self.gravatarID = gravatarID
+      self.url = url
+      self.avatarURL = avatarURL
+    }
+  }
+}
