aadf8a90e6a05ed6f7e87bf9adeac13aaf3bd55f
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-08-10 03:57:46 +0900

    fix repository owner

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-http-types", from: "1.3.0"),
     .package(url: "https://github.com/pointfreeco/swift-tagged", from: "0.10.0"),
-    .package(url: "https://github.com/apple/swift-testing", from: "0.11.0"),
+    .package(url: "https://github.com/swiftlang/swift-testing", from: "0.11.0"),
   ],
   targets: [
     .target(
